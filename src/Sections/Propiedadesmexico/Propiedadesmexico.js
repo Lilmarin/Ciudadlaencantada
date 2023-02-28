e4248cd5c126de4afb04a9a9cdd8ef3ff6752f04
fer0919 <fernandap0919@gmail.com>
2023-02-28 16:52:27 -0600

    propiedades_mexico

--- src/Sections/Propiedadesmexico/Propiedadesmexico.js
+++ src/Sections/Propiedadesmexico/Propiedadesmexico.js
@@ -1,7 +1,70 @@
 import React from "react";
+import "./Propiedadesmexico.scss";
+
+let propiedades_mexico = require('../../assets/img/CIUDAD_ENCANTADA_PROPIEDADES_MEXICO_pm-logo.png');
+let imgDiv2 = require('../../assets/img/CIUDAD_ENCANTADA_PROPIEDADES_MEXICO_Rectangle 863.png');
+let icon1 = require('../../assets/img/CIUDAD_ENCANTADA_PROPIEDADES_MEXICO_ICONO1.png');
+let icon2 = require('../../assets/img/CIUDAD_ENCANTADA_PROPIEDADES_MEXICO_ICONO2.png');
+let icon3 = require('../../assets/img/CIUDAD_ENCANTADA_PROPIEDADES_MEXICO_ICONO3.png');
+let icon4 = require('../../assets/img/CIUDAD_ENCANTADA_PROPIEDADES_MEXICO_ICONO4.png');
+let icon5 = require('../../assets/img/CIUDAD_ENCANTADA_PROPIEDADES_MEXICO_ICONO5.png');
 
 const Propiedadesmexico = () => {
-  return <div>Propiedadesmexico</div>;
+  return (
+  <section className="Section-Propiedadesmexico">
+      <div id="sizeContent2">
+      <img src={propiedades_mexico} id="sizeimage" className="padd"/>
+      <div>
+      <div id="contenedor">
+        <div className="sizecolumn">
+          <img src={icon1} id="sizeicon"></img>
+        </div>
+        <div>
+          <p>Contamos con un gran equipo de asesores</p>
+        </div>
+      </div>
+      <div id="contenedor">
+        <div className="sizecolumn">
+          <img src={icon2} id="sizeicon"></img>
+        </div>
+        <div>
+          <p>Asesoría inmobiliaria profesional y calificada</p>
+        </div>
+      </div>
+      <div id="contenedor">
+        <div className="sizecolumn">
+          <img src={icon3} id="sizeicon"></img>
+        </div>
+        <div>
+        <p>Oficinas en Torreón y en Ciudad La Encantada</p>
+        </div>
+      </div>
+      <div id="contenedor">
+        <div className="sizecolumn">
+          <img src={icon4} id="sizeicon"></img>
+        </div>
+        <div>
+        <p>Tramitamos créditos con la tasa más baja del mercado</p>
+        </div>
+      </div>
+      <div id="contenedor">
+        <div className="sizecolumn">
+          <img src={icon5} id="sizeicon"></img>
+        </div>
+        <div>
+        <p>Crédito propio </p>
+        </div>
+      </div>
+
+      </div>
+      </div>
+      <div id="sizeContent">
+      <img src={imgDiv2} id="sizeimage2"></img>
+      </div>
+    
+
+  </section>
+  );
 };
 
 export default Propiedadesmexico;
