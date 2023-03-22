37af4671f86fe344d0e67000b7241afa25389977
fer0919 <fernandap0919@gmail.com>
2023-03-21 19:38:07 -0600

    gale

--- src/Sections/Galeria/Galeria.js
+++ src/Sections/Galeria/Galeria.js
@@ -1,12 +1,31 @@
-import React from "react";
 import "./Galeria.scss";
 
-const Galeria = ({ myRef }) => {
+import React, { useState } from "react";
+
+function Galeria(props) {
+ 
+  
+  const [indice, setIndice] = useState(0);
+
   return (
-    <div ref={myRef} className="Galeria-Container">
-      Galeria
+    <div className="carrusel .Galeria-Container">
+      <img src={props.imagenes[indice]} alt={`Imagen ${indice}`} />
+      <div className="botones">
+        <button
+          onClick={() => setIndice((indice - 1 + props.imagenes.length) % props.imagenes.length)}
+        >
+          &lt;
+        </button>
+        <button
+          onClick={() => setIndice((indice + 1) % props.imagenes.length)}
+        >
+          &gt;
+        </button>
+      </div>
     </div>
   );
-};
+}
+
+
 
 export default Galeria;
