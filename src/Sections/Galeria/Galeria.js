d6dfc1d63cb81118842f9ff7f2184850b512b841
fer0919 <fernandap0919@gmail.com>
2023-03-21 19:40:12 -0600

    gale

--- src/Sections/Galeria/Galeria.js
+++ src/Sections/Galeria/Galeria.js
@@ -5,25 +5,7 @@ import React, { useState } from "react";
 function Galeria(props) {
  
   
-  const [indice, setIndice] = useState(0);
-
-  return (
-    <div className="carrusel .Galeria-Container">
-      <img src={props.imagenes[indice]} alt={`Imagen ${indice}`} />
-      <div className="botones">
-        <button
-          onClick={() => setIndice((indice - 1 + props.imagenes.length) % props.imagenes.length)}
-        >
-          &lt;
-        </button>
-        <button
-          onClick={() => setIndice((indice + 1) % props.imagenes.length)}
-        >
-          &gt;
-        </button>
-      </div>
-    </div>
-  );
+ 
 }
 
 
